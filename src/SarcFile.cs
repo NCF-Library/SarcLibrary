bce0d89acf5a5f510fad41abdd40739990941bbf
ArchLeaders <archparliament@gmail.com>
2022-12-21 01:57:38 -0800

    Re-implement SarcFile IO

--- src/SarcFile.cs
+++ src/SarcFile.cs
@@ -1,77 +1,232 @@
-﻿using Nintendo.Sarc.Parser;
-using Syroot.BinaryData.Core;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Text;
+using static SarcLibrary.SarcHelper;
 
-namespace Nintendo.Sarc
+namespace SarcLibrary
 {
-    public class SarcFile
+    public enum Endian : ushort { Big = 0xFFFE, Little = 0xFEFF }
+
+    public class SarcFile : Dictionary<string, byte[]>
     {
-        internal SarcFile() { }
-        public SarcFile(Stream stream) => Setter(SARC.DecompileSarc(stream));
-        public SarcFile(string file)
+        public Endian Endian { get; set; }
+        public bool HashOnly { get; set; } = false;
+        public bool Legacy { get; set; } = false;
+
+        public SarcFile()
         {
-            using FileStream stream = File.OpenRead(file);
-            Setter(SARC.DecompileSarc(stream));
-        }
-        public SarcFile(byte[] bytes)
-        {
-            using MemoryStream stream = new(bytes);
-            Setter(SARC.DecompileSarc(stream));
-        }
-        public SarcFile(Dictionary<string, byte[]> files, Endian endianness = Endian.Little, bool hashOnly = false)
-        {
-            Files = files;
-            Endianness = endianness;
-            HashOnly = hashOnly;
+            Endian = BitConverter.IsLittleEndian ? Endian.Little : Endian.Big;
         }
 
-        internal void Setter(SarcFile sarc)
+        public SarcFile(Stream stream)
         {
-            Files = sarc.Files;
-            Endianness = sarc.Endianness;
-            HashOnly = sarc.HashOnly;
+            using BinaryReader reader = new(stream);
+
+            Span<byte> sarc = stackalloc byte[4];
+            stream.Read(sarc);
+            if (!sarc.SequenceEqual(SARC)) {
+                throw new InvalidDataException("Invalid SARC magic");
+            }
+
+            stream.Seek(2, SeekOrigin.Current);
+            Endian = (Endian)reader.ReadUInt16();
+
+            int fileSize = reader.ReadInt32();
+            int dataOffset = reader.ReadInt32();
+            stream.Seek(10, SeekOrigin.Current);
+
+            ushort count = reader.ReadUInt16();
+            stream.Seek(4, SeekOrigin.Current);
+
+            if (Endian == Endian.Big) {
+                fileSize.SwapEndian();
+                dataOffset.SwapEndian();
+                count.SwapEndian();
+            }
+
+            Span<(uint, int, int, int)> nodes = stackalloc (uint, int, int, int)[count];
+
+            for (int i = 0; i < count; i++) {
+                uint hash = reader.ReadUInt32();
+                int attributes = reader.ReadInt32();
+                int dataStart = reader.ReadInt32();
+                int dataEnd = reader.ReadInt32();
+
+                if (Endian == Endian.Big) {
+                    hash.SwapEndian();
+                    attributes.SwapEndian();
+                    dataStart.SwapEndian();
+                    dataEnd.SwapEndian();
+                }
+
+                HashOnly = (byte)(attributes >> 24) != 1;
+                int strOffset = (attributes & 0xFFFF) * 4;
+
+                nodes[i] = (hash, strOffset, dataStart, dataEnd);
+            }
+
+            stream.Seek(8, SeekOrigin.Current);
+
+            if (!HashOnly) {
+                Span<byte> stringTableBuffer = reader.ReadBytes((int)(dataOffset - stream.Position)).AsSpan();
+                for (int i = 0; i < count; i++) {
+                    if (i == count - 1) {
+                        Add(Encoding.UTF8.GetString(stringTableBuffer[nodes[i].Item2..]).Replace(Null, Empty),
+                           reader.ReadBytes(nodes[i].Item4 - nodes[i].Item3));
+                    }
+                    else {
+                        Add(Encoding.UTF8.GetString(stringTableBuffer[nodes[i].Item2..nodes[i + 1].Item2]).Replace(Null, Empty),
+                           reader.ReadBytes(nodes[i].Item4 - nodes[i].Item3));
+                        stream.Seek(nodes[i + 1].Item3 - nodes[i].Item4, SeekOrigin.Current);
+                    }
+                }
+            }
+            else {
+                stream.Seek(dataOffset, SeekOrigin.Begin);
+                for (int i = 0; i < count; i++) {
+                    byte[] buffer = reader.ReadBytes(nodes[i].Item4 - nodes[i].Item3);
+                    Add($"{nodes[i].Item1:x8}.{GuessFileExtension(buffer)}", buffer);
+
+                    if (i != count - 1) {
+                        stream.Seek(nodes[i + 1].Item3 - nodes[i].Item4, SeekOrigin.Current);
+                    }
+                }
+            }
         }
 
-        /// <summary>
-        /// Gets or sets the archived file dictionary.
-        /// </summary>
-        public Dictionary<string, byte[]> Files { get; set; } = new();
-
-        /// <summary>
-        /// Gets or sets the SARC endianness.
-        /// </summary>
-        public Endian Endianness { get; set; } = Endian.Little;
-
-        /// <summary>
-        /// Gets or sets whether or not to only use hashed file names.
-        /// </summary>
-        public bool HashOnly { get; set; }
-
-        /// <summary>
-        /// Compile to an array of bytes.
-        /// </summary>
-        /// <returns></returns>
-        public byte[] ToBinary() => SARC.CompileSarc(this).Value;
-
-        /// <summary>
-        /// Save to a file.
-        /// </summary>
-        /// <param name="fileName"></param>
-        public void ToBinary(string fileName) => File.WriteAllBytes(fileName, SARC.CompileSarc(this).Value);
-
-        public static SarcFile FromBinary(string fileName)
+        public static SarcFile FromBinary(byte[] data)
         {
-            using FileStream stream = File.OpenRead(fileName);
-            return SARC.DecompileSarc(stream);
+            using MemoryStream ms = new(data);
+            return new(ms);
         }
-        public static SarcFile FromBinary(byte[] bytes)
-        {
-            using MemoryStream stream = new(bytes);
-            return SARC.DecompileSarc(stream);
-        }
-        public static SarcFile FromBinary(Stream stream) => SARC.DecompileSarc(stream);
 
-        public static byte[] ToBinary(SarcFile sarc) => SARC.CompileSarc(sarc).Value;
+        public static SarcFile FromBinary(string sarcFile)
+        {
+            using FileStream fs = File.OpenRead(sarcFile);
+            return new(fs);
+        }
+
+        public static SarcFile FromBinary(Stream stream)
+        {
+            return new(stream);
+        }
+
+        public byte[] ToBinary()
+        {
+            using MemoryStream ms = new();
+            ToBinary(ms);
+            return ms.ToArray();
+        }
+
+        public void ToBinary(string outputFile)
+        {
+            using FileStream fs = File.Create(outputFile);
+            ToBinary(fs);
+        }
+
+        public void ToBinary(Stream stream)
+        {
+            using BinaryWriter writer = new(stream);
+
+            // Allocate sorted keys/values
+            string[] fileNames = Keys.ToArray();
+            uint[] keys = new uint[Count];
+            (string, int, int, int, int)[] nodes = new (string, int, int, int, int)[Count];
+
+            int relStringOffset = 0;
+            int relDataOffset = 0;
+            int fileAlignment = 1;
+            for (int i = 0; i < Count; i++) {
+                string fileName = fileNames[i];
+                keys[i] = GetHash(fileName);
+
+                // Calculate data and string offsets
+                Span<byte> buffer = this[fileName].AsSpan();
+                int alignment = GetFileAlignment(fileName, buffer, this);
+                int dataStart = relDataOffset.Align(alignment);
+                int dataEnd = dataStart + buffer.Length;
+                nodes[i] = (fileName, dataStart, dataEnd, relStringOffset / 4, alignment);
+
+                relDataOffset = dataEnd;
+                relStringOffset += fileName.Length + 4 & -4;
+                fileAlignment = LCM(fileAlignment, alignment);
+            }
+
+            Array.Sort(keys, nodes);
+
+            stream.Seek(0x14, SeekOrigin.Begin);
+
+            // Write data nodes (SFAT)
+            writer.Write(SFAT);
+            writer.Write(0x0C.AsInt16(Endian));
+            writer.Write(Count.AsInt16(Endian));
+            writer.Write(HashKey.AsUInt32(Endian));
+
+            for (int i = 0; i < Count; i++) {
+                writer.Write(keys[i].AsUInt32(Endian));
+                writer.Write(HashOnly ? 0x00 : (0x01000000 | nodes[i].Item4).AsInt32(Endian));
+                writer.Write(nodes[i].Item2.AsInt32(Endian));
+                writer.Write(nodes[i].Item3.AsInt32(Endian));
+            }
+
+            // Write string table (SFNT)
+            writer.Write(SFNT);
+            writer.Write(0x08.AsInt16(Endian));
+            writer.Write((ushort)0x00);
+
+            for (int i = 0; i < Count; i++) {
+                byte[] buffer = Encoding.UTF8.GetBytes(nodes[i].Item1);
+                byte[] aligned = new byte[buffer.Length + 4 & -4];
+                Array.Copy(buffer, aligned, buffer.Length);
+                writer.Write(aligned);
+            }
+
+            // Console.WriteLine(string.Join(", ", nodes.Select(x => x.Item4)));
+            stream.Align(fileAlignment);
+            int dataOffset = (int)stream.Position;
+
+            // Write data
+            for (int i = 0; i < Count; i++) {
+                stream.Align(nodes[i].Item5);
+                writer.Write(this[nodes[i].Item1]);
+            }
+
+            int fileSize = (int)stream.Position;
+
+            stream.Seek(0, SeekOrigin.Begin);
+            writer.Write(SARC);
+            writer.Write(0x14.AsInt16(Endian));
+            writer.Write((ushort)Endian);
+            writer.Write(fileSize.AsInt32(Endian));
+            writer.Write(dataOffset.AsInt32(Endian)); // Data offset (0x0C)
+            writer.Write(0x0100.AsInt16(Endian)); // SARC Version
+            writer.Write((ushort)0x00); // Reserved
+        }
+
+        public async Task ExtractToDirectory(string outputDirectory)
+        {
+            if (HashOnly) {
+                Directory.CreateDirectory(outputDirectory);
+            }
+
+            await Parallel.ForEachAsync(this, async (sarcFile, cancellationToken) => {
+                if (!HashOnly) {
+                    string file = Path.Combine(outputDirectory, sarcFile.Key);
+                    Directory.CreateDirectory(Path.GetDirectoryName(file) ?? "");
+                    await File.WriteAllBytesAsync(file, sarcFile.Value, cancellationToken);
+                }
+                else {
+                    await File.WriteAllBytesAsync(Path.Combine(outputDirectory, sarcFile.Key), sarcFile.Value, cancellationToken);
+                }
+            });
+        }
+
+        public static SarcFile LoadFromDirectory(string directory, string searchPattern = "*.*", SearchOption searchOption = SearchOption.AllDirectories)
+        {
+            SarcFile sarc = new();
+            foreach (var file in Directory.EnumerateFiles(directory, searchPattern, searchOption)) {
+                sarc.Add(Path.GetRelativePath(directory, file), File.ReadAllBytes(file));
+            }
+
+            return sarc;
+        }
     }
 }
---
+++ src/SarcHelper.cs
@@ -0,0 +1,197 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SarcLibrary
+{
+    public static partial class SarcHelper
+    {
+        internal static readonly byte[] SARC = { 0x53, 0x41, 0x52, 0x43 };
+        internal static readonly byte[] SFAT = { 0x53, 0x46, 0x41, 0x54 };
+        internal static readonly byte[] SFNT = { 0x53, 0x46, 0x4E, 0x54 };
+        internal static readonly byte[] FLIM = { 0x46, 0x4C, 0x49, 0x4D };
+        internal static readonly byte[] YAZ0 = { 0x59, 0x41, 0x5A, 0x30 };
+
+        internal const string Null = "\x00";
+        internal const string Empty = "";
+        internal const string BFFNT = "bffnt";
+
+        internal const uint HashKey = 0x65;
+        internal const int MinAlignment = 4;
+
+        private static readonly Regex RegexAZ = CompiledRegexAZ();
+
+        private static readonly Dictionary<string, string> FileExtensions = new() {
+            { "AAHS", ".sharc" }, { "AAMP", ".aamp" }, { "BAHS", ".sharcb" },
+            { "BNSH", ".bnsh" }, { "BNTX", ".bntx" }, { "BY", ".byaml" },
+            { "CFNT", ".bcfnt" }, { "CGFX", ".bcres" }, { "CLAN", ".bclan" },
+            { "CLYT", ".bclyt" }, { "CSTM", ".bcstm" }, { "CTPK", ".ctpk" },
+            { "CWAV", ".bcwav" }, { "FFNT", ".bffnt" }, { "FLAN", ".bflan" },
+            { "FLIM", ".bclim" }, { "FLYT", ".bflyt" }, { "FRES", ".bfres" },
+            { "FSEQ", ".bfseq" }, { "FSHA", ".bfsha" }, { "FSTM", ".bfstm" },
+            { "FWAV", ".bfwav" }, { "Gfx2", ".gtx" }, { "MsgPrjBn", ".msbp" },
+            { "MsgStdBn", ".msbt" }, { "SARC", ".sarc" }, { "STM", ".bfsha" },
+            { "VFXB", ".pctl" }, { "Yaz", ".szs" }, { "YB", ".byaml" },
+        };
+
+        private static readonly Dictionary<string, int> FileAlignments = new() {
+            { "aglatex", 8 }, { "baglatex", 8 }, { "aglblm", 8 }, { "baglblm", 8 },
+            { "aglccr", 8 }, { "baglccr", 8 }, { "aglclwd", 8 }, { "baglclwd", 8 },
+            { "aglcube", 8 }, { "baglcube", 8 }, { "agldof", 8 }, { "bagldof", 8 },
+            { "aglenv", 8 }, { "baglenv", 8 }, { "aglenvset", 8 }, { "baglenvset", 8 },
+            { "aglfila", 8 }, { "baglfila", 8 }, { "agllmap", 8 }, { "bagllmap", 8 },
+            { "agllref", 8 }, { "bagllref", 8 }, { "aglshpp", 8 }, { "baglshpp", 8 },
+            { "glght", 8 }, { "bglght", 8 }, { "glpbd", 8 }, { "bglpbd", 8 },
+            { "glpbm", 8 }, { "bglpbm", 8 }, { "gsdw", 8 }, { "bgsdw", 8 },
+            { "ksky", 8 }, { "bksky", 8 }, { "ofx", -8192 }, { "bofx", -8192 },
+            { "pref", 8 }, { "bpref", 8 }, { "sharc", 0x1000 }, { "sharcb", 0x1000 },
+            { "baglmf", 0x80 }, { "fmd", -8192 }, { "ftx", -8192 }, { "genvres", -8192 },
+            { "gtx", 0x2000 }
+        };
+
+        private static readonly string[] BotwFactoryNames = {
+            "sarc", "bfres", "bcamanim", "batpl, bnfprl", "bplacement",
+            "hks, lua", "bactcapt", "bitemico", "jpg", "bmaptex",
+            "bstftex", "bgdata", "bgsvdata", "hknm2", "bmscdef", "bars",
+            "bxml", "bgparamlist", "bmodellist", "baslist", "baiprog", "bphysics",
+            "bchemical", "bas", "batcllist", "batcl", "baischedule", "bdmgparam",
+            "brgconfiglist", "brgconfig", "brgbw", "bawareness", "bdrop", "bshop",
+            "brecipe", "blod", "bbonectrl", "blifecondition", "bumii", "baniminfo",
+            "byaml", "bassetting", "hkrb", "hkrg", "bphyssb", "hkcl", "hksc",
+            "hktmrb", "brgcon", "esetlist", "bdemo", "bfevfl", "bfevtm"
+        };
+
+        public static string GuessFileExtension(ReadOnlySpan<byte> data)
+        {
+            string magic = data[0..8].SequenceEqual(YAZ0) ? Encoding.UTF8.GetString(data[0x11..0x15]) : Encoding.UTF8.GetString(data[0..8]);
+            return FileExtensions.TryGetValue(RegexAZ.Replace(magic, Empty), out string? value) ? value : "bin";
+        }
+
+        public static int GetBinaryFileAlignment(ReadOnlySpan<byte> data)
+        {
+            if (data.Length <= 0x20) {
+                return 1;
+            }
+
+            Endian endian = (Endian)BitConverter.ToInt16(data[0x0C..0x0E]);
+
+            int fileSize = BitConverter.ToInt32(data[0x1C..0x20]).AsInt32(endian);
+            if (fileSize != data.Length) {
+                return 1;
+            }
+
+            return 1 << data[0x0E];
+        }
+
+        public static int GetCafeBflimAlignment(ReadOnlySpan<byte> data)
+        {
+            if (data.Length <= 0x28 || !data[^0x28..^0x24].SequenceEqual(FLIM)) {
+                return 1;
+            }
+
+            Endian endian = (Endian)BitConverter.ToInt16(data[^0x24..^0x22]);
+            return BitConverter.ToInt16(data[^0x8..^0x06]).AsInt32(endian);
+        }
+
+        public static int GetFileAlignment(string name, ReadOnlySpan<byte> data, SarcFile sarc)
+        {
+            int alignment = MinAlignment;
+            string ext = Path.GetExtension(name).Remove(0, 1);
+
+            if (ext == BFFNT) {
+                alignment = LCM(alignment, sarc.Endian == Endian.Big ? 0x2000 : 0x1000);
+            }
+            else if (FileAlignments.TryGetValue(ext, out int fetchedAlignment)) {
+                alignment = LCM(alignment, fetchedAlignment);
+            }
+
+            if (sarc.Legacy && (data[0x00..0x04].SequenceEqual(SARC) || data[0x00..0x04].SequenceEqual(YAZ0) && data[0x11..0x15].SequenceEqual(SARC))) {
+                alignment = LCM(alignment, 0x2000);
+            }
+
+            if (sarc.Legacy || Array.IndexOf(BotwFactoryNames, ext) == -1) {
+                alignment = LCM(alignment, GetBinaryFileAlignment(data));
+                if (sarc.Endian == Endian.Big) {
+                    alignment = LCM(alignment, GetCafeBflimAlignment(data));
+                }
+            }
+
+            return alignment;
+        }
+
+        public static int GCD(int a, int b)
+        {
+            while (a != 0 && b != 0) {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            return a | b;
+        }
+
+        public static int LCM(int a, int b)
+        {
+            return a / GCD(a, b) * b;
+        }
+
+        public static uint GetHash(ReadOnlySpan<char> name)
+        {
+            long hash = 0;
+            for (int i = 0; i < name.Length; i++) {
+                hash = hash * HashKey + (sbyte)name[i];
+            }
+
+            return (uint)hash;
+        }
+
+        public static void SwapEndian(ref this int value)
+        {
+            value = (int)(((value & 0x000000FF) << 24) + ((value & 0x0000FF00) << 8) + ((value & 0x00FF0000) >> 8) + ((value & 0xFF000000) >> 24));
+        }
+
+        public static void SwapEndian(ref this uint value)
+        {
+            value = ((value & 0x000000FF) << 24) + ((value & 0x0000FF00) << 8) + ((value & 0x00FF0000) >> 8) + ((value & 0xFF000000) >> 24);
+        }
+
+        public static void SwapEndian(ref this ushort value)
+        {
+            value = (ushort)(((value & 0x00FF) << 8) + ((value & 0xFF00) >> 8));
+        }
+
+        public static int AsInt32(this short value, Endian endian)
+        {
+            return endian == Endian.Big ? ((value & 0x00FF) << 8) + ((value & 0xFF00) >> 8) : value;
+        }
+
+        public static int AsInt32(this int value, Endian endian)
+        {
+            return endian == Endian.Big ? (int)(((value & 0x000000FF) << 24) + ((value & 0x0000FF00) << 8) + ((value & 0x00FF0000) >> 8) + ((value & 0xFF000000) >> 24)) : value;
+        }
+
+        public static uint AsUInt32(this uint value, Endian endian)
+        {
+            return endian == Endian.Big ? ((value & 0x000000FF) << 24) + ((value & 0x0000FF00) << 8) + ((value & 0x00FF0000) >> 8) + ((value & 0xFF000000) >> 24) : value;
+        }
+
+        public static ushort AsInt16(this int value, Endian endian)
+        {
+            return (ushort)(endian == Endian.Big ? ((value & 0x00FF) << 8) + ((value & 0xFF00) >> 8) : value);
+        }
+
+        public static void Align(this Stream stream, int alignment)
+        {
+            byte[] buffer = new byte[(alignment - (int)stream.Position % alignment) % alignment];
+            stream.Write(buffer);
+        }
+
+        public static int Align(this int value, int alignment)
+        {
+            return value + (alignment - value % alignment) % alignment;
+        }
+
+        [GeneratedRegex("[^a-zA-Z0-9 -]")]
+        private static partial Regex CompiledRegexAZ();
+    }
+}
