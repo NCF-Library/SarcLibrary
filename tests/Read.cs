221f7850068a366c2b54f94795f0a73f6973ece6
ArchLeaders <archparliament@gmail.com>
2022-12-21 02:00:26 -0800

    Create IO uint tests

---
+++ tests/Read.cs
@@ -0,0 +1,21 @@
+﻿using SarcLibrary;
+
+namespace Tests
+{
+    [TestClass]
+    public class Read
+    {
+        [TestMethod]
+        [DataRow("D:\\Botw\\Cemu (Stable)\\mlc01\\usr\\title\\0005000e\\101c9500\\content\\Pack\\TitleBG.pack")]
+        [DataRow("D:\\Botw\\Files\\01007EF00011E000\\romfs\\Pack\\TitleBG.pack")]
+        public void ReadSarc(string sarc)
+        {
+            using FileStream fs = File.OpenRead(sarc);
+            SarcFile sarcBase = new(fs);
+
+            foreach ((var file, var _) in sarcBase) {
+                Console.WriteLine(file);
+            }
+        }
+    }
+}
---
+++ tests/Write.cs
@@ -0,0 +1,23 @@
+﻿using SarcLibrary;
+
+namespace Tests
+{
+    [TestClass]
+    public class Write
+    {
+        [TestMethod]
+        [DataRow("D:\\Botw\\Cemu (Stable)\\mlc01\\usr\\title\\0005000e\\101c9500\\content\\Pack\\TitleBG.pack")]
+        [DataRow("D:\\Botw\\Files\\01007EF00011E000\\romfs\\Pack\\TitleBG.pack")]
+        public void WriteSarc(string sarc)
+        {
+            SarcFile sarcBase;
+            using (FileStream fs = File.OpenRead(sarc)) {
+                sarcBase = new(fs);
+            }
+
+            using (FileStream fs = File.Create(sarc + ".bin")) {
+                sarcBase.ToBinary(fs);
+            }
+        }
+    }
+}
