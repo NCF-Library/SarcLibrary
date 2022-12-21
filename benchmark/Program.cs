36201b85e5caa32f83b94827f2deef67ab9e016c
ArchLeaders <archparliament@gmail.com>
2022-12-21 02:00:59 -0800

    Create IO benchmarks

---
+++ benchmark/Program.cs
@@ -0,0 +1,32 @@
+﻿#if DEBUG
+
+using SarcLibrary;
+
+SarcBenchmark sb = new();
+Dictionary<string, BenchmarkData> benchmarkData = new();
+
+await sb.Setup();
+
+foreach (var func in sb.GetType().GetMethods().Where(x => x.GetCustomAttributes<BenchmarkAttribute>().Any())) {
+    benchmarkData.Add(func.Name, new BenchmarkData(new(), new()));
+    for (int i = 0; i < 5; i++) {
+        Stopwatch watch = Stopwatch.StartNew();
+        func.Invoke(sb, Array.Empty<object>());
+        watch.Stop();
+        benchmarkData[func.Name].Ticks.Add(watch.ElapsedTicks);
+        benchmarkData[func.Name].Milliseconds.Add(watch.ElapsedMilliseconds);
+    }
+}
+
+foreach ((var name, var data) in benchmarkData) {
+    Console.WriteLine($"{name}:");
+    Console.WriteLine($"Ticks: {data.Ticks.Min()}:{data.Ticks.Max()} | {string.Join(", ", data.Ticks)}");
+    Console.WriteLine($"Milliseconds: {data.Milliseconds.Min()}:{data.Milliseconds.Max()} | {string.Join(", ", data.Milliseconds)}");
+    Console.WriteLine();
+}
+
+#else
+
+BenchmarkRunner.Run<SarcBenchmark>();
+
+#endif
---
+++ benchmark/SarcBenchmark.cs
@@ -0,0 +1,81 @@
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+using BenchmarkDotNet.Attributes;
+using SarcLibrary;
+
+namespace Benchmark
+{
+    public record BenchmarkData(List<long> Ticks, List<long> Milliseconds);
+
+    [MemoryDiagnoser]
+    public class SarcBenchmark
+    {
+        private readonly string _sarcBePath = "F:\\Bin\\wiiu\\TitleBG.pack";
+        private readonly string _sarcLePath = "F:\\Bin\\nx\\TitleBG.pack";
+
+        private SarcFile _sarcBe;
+        private SarcFile _sarcLe;
+
+        [GlobalSetup]
+        public async Task Setup()
+        {
+            using (FileStream fs = File.OpenRead(_sarcBePath)) {
+                _sarcBe = new(fs);
+                await _sarcBe.ExtractToDirectory($"{_sarcBePath}.setup.ex");
+            }
+
+            using (FileStream fs = File.OpenRead(_sarcLePath)) {
+                _sarcLe = new(fs);
+                await _sarcLe.ExtractToDirectory($"{_sarcLePath}.setup.ex");
+            }
+        }
+
+        [Benchmark]
+        public void SarcReadBE()
+        {
+            SarcFile sarc = SarcFile.FromBinary(_sarcBePath);
+        }
+
+        [Benchmark]
+        public void SarcWriteBE()
+        {
+            _sarcBe.ToBinary($"{_sarcBePath}.pack");
+        }
+
+        [Benchmark]
+        public async Task SarcExtractBE()
+        {
+            await _sarcBe.ExtractToDirectory($"{_sarcBePath}.ex");
+        }
+
+        [Benchmark]
+        public void SarcLoadBE()
+        {
+            SarcFile sarc = SarcFile.LoadFromDirectory($"{_sarcBePath}.setup.ex");
+        }
+
+        [Benchmark]
+        public void SarcReadLE()
+        {
+            SarcFile sarc = SarcFile.FromBinary(_sarcLePath);
+        }
+
+        [Benchmark]
+        public void SarcWriteLE()
+        {
+            _sarcLe.ToBinary($"{_sarcLePath}.pack");
+        }
+
+        [Benchmark]
+        public async Task SarcExtractLE()
+        {
+            await _sarcLe.ExtractToDirectory($"{_sarcLePath}.ex");
+        }
+
+        [Benchmark]
+        public void SarcLoadLE()
+        {
+            SarcFile sarc = SarcFile.LoadFromDirectory($"{_sarcLePath}.setup.ex");
+        }
+    }
+}
